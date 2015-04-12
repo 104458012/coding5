6695ca0e645b1a883644dcf53a251371e6c89c71
ndoing <service@echoo.tw>
2015-04-12 11:17:23 +0800

    update Main.java add note

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,12 @@
+/*
+* Date: 2015-04-12
+* Authore: Coding5
+* Purpose: Demo coding5 java main program
+* Web: http://www.coding5.com
+*/
+
 public class main{
-	public static void main(String[] args){
-		System.out.print("hello world");
-	}
+  public static void main(String[] args){
+    System.out.print("hello world");
+  }
 }
