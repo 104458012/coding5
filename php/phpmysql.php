cf23b24f38ecac857c5366068e110ec9134da571
coding5 <amou.server@gmail.com>
2015-05-06 21:35:28 +0800

    add phpmysql.php

---
+++ php/phpmysql.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ Date: 2015-05-06
+ Authore: Coding5
+ Purpose: php 建立MySQL 資料庫連線 
+ Websit: http://www.coding5.com/works?transno=1000000006
+*/
+
+$dbhost = 'localhost:3306';
+$dbuser = 'root';
+$dbpass = 'p123';
+$dbname = 'DBNAME';
+
+$conn = mysql_connect($dbhost, $dbuser, $dbpass) ;
+
+mysql_query("SET NAMES 'UTF8'");
+mysql_select_db($dbname);
+
+$sql = "select codeA from coding5 "; 
+$result = mysql_query($sql) ; /* 執行SQL */
+
+/* while 迴圈撈取資料 */
+while($row = mysql_fetch_array($result)){
+  echo $row['transno']."&lt;br&gt;";
+}
+
+mysql_close($conn);
+
+?>
