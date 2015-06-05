06fac1c27c7c68eb80aaa7f361a9600c3c4687e1
coding5 <amou.server@gmail.com>
2015-06-05 11:37:29 +0800

    add go hello.go

---
+++ go/hello.go
@@ -0,0 +1,12 @@
+// Date: 2015-06-05
+// Authore: Coding5
+// Purpose: go 程式測試 
+// Websit: http://www.coding5.com/works?transno=1000000030
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("hello, go\n")
+}
